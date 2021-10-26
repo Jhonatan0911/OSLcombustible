c23f670dcf55b001f205ae4406b3eb788fce1a30
Jhonatan0911 <jpinalozano911@gmail.com>
2021-10-26 17:18:49 -0500

    home y login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes
+import { LoginComponent } from './plantillas/cuenta/login/login.component';
+import { RegisterComponent } from './plantillas/cuenta/register/register.component';
+import { HomeComponent } from './plantillas/home/home/home.component';
+
+
+const routes: Routes = [
+  //Home
+  { path: '', component: LoginComponent},
+
+  //register  
+  { path: 'register', component: RegisterComponent},
+
+  //inicio
+  { path: 'home', component: HomeComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top',
+    })  
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './plantillas/cuenta/login/login.component';
+import { RegisterComponent } from './plantillas/cuenta/register/register.component';
+import { HomeComponent } from './plantillas/home/home/home.component';
+import { HeaderComponent } from './componentes/header/header/header.component';
+import { FooterComponent } from './componentes/footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
