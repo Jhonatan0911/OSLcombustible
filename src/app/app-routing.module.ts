768b9f967967a8dfa16ccce5a09c85400df8dcc1
Jhonatan0911 <jpinalozano911@gmail.com>
2021-10-27 07:58:14 -0500

    routing en los componentes del home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './plantillas/cuenta/login/login.component';
 import { RegisterComponent } from './plantillas/cuenta/register/register.component';
 import { HomeComponent } from './plantillas/home/home/home.component';
+//vistas
+import { InformeComponent } from './plantillas/informe/informe/informe.component';
+import { OperacionComponent } from './plantillas/operacion/operacion/operacion.component';
+import { RegistroComponent } from './plantillas/registro/registro/registro.component';
+import { UsuarioComponent } from './plantillas/cliente/usuario/usuario.component';
+import { UserComponent } from './plantillas/cliente/placa/user/user.component';
 
 
 const routes: Routes = [
@@ -12,10 +18,26 @@ const routes: Routes = [
   { path: '', component: LoginComponent},
 
   //register  
-  { path: 'register', component: RegisterComponent},
+  { path: 'signup', component: RegisterComponent},
 
   //inicio
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+
+  //vista en la que se decarga el pdf
+  { path: 'informe', component: InformeComponent},
+
+  //vista en la que se crea una nueva operacion
+  { path: 'operacion', component: OperacionComponent},
+
+  //vista en la que se crea un registro nuevo
+  { path: 'registro', component: RegistroComponent},
+
+  //vista en la que se osbservan los usuarios
+  { path: 'usuarios', component: UsuarioComponent},
+
+  //vista en la que se crean los usuarios
+  { path: 'user', component: UsuarioComponent},
+
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { RegisterComponent } from './plantillas/cuenta/register/register.compone
 import { HomeComponent } from './plantillas/home/home/home.component';
 import { HeaderComponent } from './componentes/header/header/header.component';
 import { FooterComponent } from './componentes/footer/footer/footer.component';
+import { InformeComponent } from './plantillas/informe/informe/informe.component';
+import { OperacionComponent } from './plantillas/operacion/operacion/operacion.component';
+import { RegistroComponent } from './plantillas/registro/registro/registro.component';
+import { UsuarioComponent } from './plantillas/cliente/usuario/usuario.component';
+import { UserComponent } from './plantillas/cliente/placa/user/user.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { FooterComponent } from './componentes/footer/footer/footer.component';
     RegisterComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    InformeComponent,
+    OperacionComponent,
+    RegistroComponent,
+    UsuarioComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
